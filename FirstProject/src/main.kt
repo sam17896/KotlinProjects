0beda1f7d07af06d4ab269379041b27cd7f2a8b4
Ahsan Sohail <k142109@nu.edu.pk>
2017-07-01 19:20:17 +0500

    Conditions

--- FirstProject/src/main.kt
+++ FirstProject/src/main.kt
@@ -110,6 +110,30 @@ fun main(args : Array<String>){
     for(x in rng3) println("rng3 : $x")
     for(x in tenTo1.reversed()) println("Reverse : $x")
 
-    
+    //Conditions
 
+    val age = 8
+    if(age < 5){
+        println("Go to PreSchool")
+    } else if (age==5){
+        println("Go to Kindergarten")
+    } else if ((age > 5) && (age<=17)){
+        val grade = age - 5
+        println("Go to Grade $grade")
+    } else {
+        println("Go to College")
+    }
+
+    when (age){
+        0,1,2,3,4 -> println("Go to PreSchool")
+        5 -> println("Go to Kindergarten")
+
+        in 6..17 -> {
+            val grade = age-5
+            println("Go to Grade $grade")
+        }
+
+        else -> println("Go to College")
+
+    }
 }
