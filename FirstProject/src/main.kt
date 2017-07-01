20a9cd08a72adf642864df38530c94a1e260d53d
Ahsan Sohail <k142109@nu.edu.pk>
2017-07-01 19:11:31 +0500

    Ranges

--- FirstProject/src/main.kt
+++ FirstProject/src/main.kt
@@ -100,8 +100,16 @@ fun main(args : Array<String>){
     println(sqArray[2])
     var arr2 : Array<Int> = arrayOf(1,2,3)
 
+    //Ranges
+    val oneTo10  = 1..10
+    val alpha  = "A".."Z"
+    println("R in alpha : ${"R" in alpha}")
+    val tenTo1 = 10.downTo(1)
+    val twoTo20 = 2.rangeTo(20)
+    val rng3 = oneTo10.step(3)
+    for(x in rng3) println("rng3 : $x")
+    for(x in tenTo1.reversed()) println("Reverse : $x")
 
     
-
 
 }
