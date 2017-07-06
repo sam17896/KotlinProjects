e67b9283d331c7c79b5511af5d7f652f740a34f1
Ahsan Sohail <k142109@nu.edu.pk>
2017-07-06 14:03:56 +0500

    Lists

--- FirstProject/src/main.kt
+++ FirstProject/src/main.kt
@@ -258,7 +258,26 @@ fun main(args : Array<String>){
     }
 
 
-    
+    //List
+
+    var list1 : MutableList<Int> = mutableListOf(1,2,3,4,5)
+    val list2 : List<Int> = listOf(1,2,3,4,5)
+
+    list1.add(6)
+    println("1st : ${list1.first()}")
+    println("last : ${list1.last()}")
+    println("2nd : ${list2[2]}")
+
+    var list3 = list1.subList(0,3)
+
+    println("Length : ${list1.size}")
+
+    list3.clear()
+    list1.remove(1)
+    list1.removeAt(2)
+
+    list1.forEach{n->println("Mutable List : $n")}
+
 
 
 }
